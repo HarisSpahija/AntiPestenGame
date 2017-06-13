688958d47c481859bfc11df1e9fa896a3b3c2be4
Conan <Conan.spithoven@gmail.com>
2017-06-13 02:45:20 +0200

    Add files via upload
    heb de code uit main.ts naar game.ts verplaatst

--- dev/game.ts
+++ dev/game.ts
@@ -6,11 +6,22 @@ var ctx = CanvasRenderingContext2D;
 
 function game()
 {
+    if (Collision.RectangleRectangle(player, collision)) {
+        //trigger event
+    }
+    else {
+        //don't trigger event
+    }
+
+    document.addEventListener('keydown', keyboardinput);
+
+
     requestAnimationFrame(game);
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, 1280, 720);
 
     canvas = <HTMLCanvasElement>document.getElementById('cnvs');
+    ctx = canvas.getContext("2d");
 
     var shape : ishape;
     for (var i: number = 0; i < shape_array.length; i++)
--- dev/main.ts
+++ dev/main.ts
@@ -1,14 +1,5 @@
+/// <reference path="game.ts"/>
+
 window.onload = () => {
-    
-    if (Collision.RectangleRectangle(player, collision)) {
-        //trigger event
-    }
-    else {
-        //don't trigger event
-    }
-
-    document.addEventListener('keydown', keyboardinput);
-
-    ctx = canvas.getContext("2d");
     game();
 }
